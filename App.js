5cd1bd08031213155c29d274422ed03e2dafe008
Pranali Ahirrao <55869366+pranaliahirrao@users.noreply.github.com>
2023-10-26 10:55:54 +0530

    React code - with some css

---
+++ App.js
@@ -0,0 +1,7 @@
+const heading = React.createElement('h1' , {id: 'heading'} , "Hello React....!");
+
+//                    props =                  attributes  +     children
+
+// console.log(heading) --> obj , this react element is nothing but Object;
+const root = ReactDOM.createRoot(document.getElementById('rooot'));
+root.render(heading);
